7af3cfbf95ec222620cbe585f06bebd4b54a5533
GO354146 <gobika.k86@wipro.com>
2017-06-13 23:45:31 +0530

    after implementing obsev

--- client/app/todo/services/task-service.ts
+++ client/app/todo/services/task-service.ts
@@ -1,6 +1,9 @@
-import {Http, Headers} from '@angular/http';
+import {Http, Response, Headers} from '@angular/http';
 import {Injectable} from "@angular/core";
+import {Task} from "../models/task";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {Observable} from 'rxjs/Rx';
 
 
 
@@ -9,9 +12,10 @@ export class TaskService {
         constructor(private http :Http){
             console.log('Task Service Initialized...');
         }
-        getTasks(){
-            return this.http.get('/tasks/viewTasks')
-                .map(res => res.json());
+        getTasks() : Observable<Task[]>{
+            return this.http.get('http://localhost:8080/tasks/viewTasks')
+                .map((res:Response) => res.json())
+                .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
         }
 
 }
