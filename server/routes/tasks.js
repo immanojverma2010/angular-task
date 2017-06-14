a25b2178dc82e75a1e8ff638ad8b5ae61d007081
manojkumar-g <manoj.g.kumar@hotmail.com>
2017-06-14 20:18:28 +0530

    All routes created

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -22,6 +22,43 @@ router.route("/viewTasks")
 
 });
 
+router.route("/deleteTask")
+.delete(function (req,res) {
+    var name=req.body.name;
+
+      TaskModel.remove({'name':name},function(err){
+        if(err)
+        {
+          res.send(err);
+        }
+        else{
+          console.log("Task Deleted from mongo");
+          res.send("Task Deleted from mongo");
+        }
+      })
+});
+
+
+
+router.route("/taskUpdate")
+.put(function(req, res){
+  console.log("put req called " +req.body.taskName+"    "+req.body.taskStatus);
+  if(req.body){
+
+    TaskModel.update({ name: req.body.taskName }, { done: req.body.taskStatus },function(err){
+      if(err)
+      {
+        res.send(err);
+      }
+      else {
+        console.log("Updated the news")
+        res.send("Updated news");
+      }
+    })
+  }
+});
+
+
 
 
 router.route("/addTask")
